fa1a77343a4c5b2fee267283119273a18725d858
jurchiks33 <jurisslisans@gmail.com>
2024-03-11 22:27:48 +0100

    work on a frames

--- sonicSplicer.py
+++ sonicSplicer.py
@@ -21,9 +21,12 @@ def main():
     # Set the initial size and position of the window.
     root.geometry(f'{width}x{height}+{x_offset}+{y_offset}')
 
-    # Make the layout responsive to window resizing.
-    root.grid_columnconfigure(0, weight=1)
-    root.grid_rowconfigure(0, weight=1)
+    # # Make the layout responsive to window resizing.
+    # root.grid_columnconfigure(0, weight=1)
+    # root.grid_rowconfigure(0, weight=1)
+    frame1 = tk.Frame(root, bg='black', bd=2, relief='solid')
+    frame2 = tk.Frame(root, bg='black', bd=2, relief='solid')
+    
 
     # Start the Tkinter event loop.
     root.mainloop()
