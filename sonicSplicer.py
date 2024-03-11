4fabad66e25de811e061de0b8f5bd568759f5580
jurchiks33 <jurisslisans@gmail.com>
2024-03-11 12:38:51 +0100

    work on a UI

--- sonicSplicer.py
+++ sonicSplicer.py
@@ -5,6 +5,9 @@ def main():
     root = tk.Tk()
     root.title("SonicSplicer - Music Editing Application")
 
+    # Background color.
+    root.configure(bg='#1a1a1a')
+
     #Calculate dimensions for the window to be 70% of screen size.
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
@@ -16,8 +19,14 @@ def main():
     y_offset = int((screen_height - height) / 2)
 
     # Set the initial size and position of the window.
-    root.geometry(f'{width} x {height} + {x_offset} + {y_offset}')
+    root.geometry(f'{width}x{height}+{x_offset}+{y_offset}')
 
     # Make the layout responsive to window resizing.
     root.grid_columnconfigure(0, weight=1)
     root.grid_rowconfigure(0, weight=1)
+
+    # Start the Tkinter event loop.
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
