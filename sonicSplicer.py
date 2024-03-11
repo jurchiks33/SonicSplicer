baa7d7b3bb84cecbd33e586ec652601e3a072eb9
jurchiks33 <jurisslisans@gmail.com>
2024-03-11 22:39:34 +0100

    adjusting frames

--- sonicSplicer.py
+++ sonicSplicer.py
@@ -31,8 +31,8 @@ def main():
     frame5 = tk.Frame(root, bg='black', bd=2, relief='solid')
 
     #Place frames in a layout.
-    frame1.place(relx=0.05, rely=0.05, relwidth=0.2, relheight=0.4)
-    frame2.place(relx=0.05, rely=0.55, relwidth=0.2, relheight=0.1)
+    frame1.place(relx=0.05, rely=0.05, relwidth=0.2, relheight=0.2)
+    frame2.place(relx=0.05, rely=0.39, relwidth=0.2, relheight=0.1)
     frame3.place(relx=0.3, rely=0.05, relwidth=0.4, relheight=0.6)
     frame4.place(relx=0.75, rely=0.05, relwidth=0.2, relheight=0.3)
     frame5.place(relx=0.75, rely=0.4, relwidth=0.2, relheight=0.2)
