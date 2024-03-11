06c411ae59366c7e54f3afb5c6be027d30be82a7
jurchiks33 <jurisslisans@gmail.com>
2024-03-11 12:32:31 +0100

    working on a main window

--- sonicSplicer.py
+++ sonicSplicer.py
@@ -10,3 +10,14 @@ def main():
     screen_height = root.winfo_screenheight()
     width = int(screen_width * 0.7)
     height = int(screen_height * 0.7)
+
+    # Center tthe window on the screen.
+    x_offset = int((screen_width - width) / 2)
+    y_offset = int((screen_height - height) / 2)
+
+    # Set the initial size and position of the window.
+    root.geometry(f'{width} x {height} + {x_offset} + {y_offset}')
+
+    # Make the layout responsive to window resizing.
+    root.grid_columnconfigure(0, weight=1)
+    root.grid_rowconfigure(0, weight=1)
