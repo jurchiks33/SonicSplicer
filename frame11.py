6be2b978c9a54c7f66353b488ab88497d92f8be3
jurchiks33 <jurisslisans@gmail.com>
2024-03-14 20:06:10 +0100

    work on a equalizer

--- frame11.py
+++ frame11.py
@@ -2,3 +2,9 @@
 import tkinter as tk
 import random
 
+class EqualizerFrame:
+    def __init__(self, parent):
+        self.frame = tk.Frame(parent, bg='black')
+        self.canvas = tk.Canvas(self.frame, bg='black')
+        self.canvas.pack(fill=tk.BOTH, expand=True)
+        self.bars = []
